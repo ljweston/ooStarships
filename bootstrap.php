a3987b5537e0b570b767108999168d1287e36253
ljweston <lweston@ncees.org>
2022-05-23 11:19:26 -0400

    Installed Composer to handle our autoloading

--- bootstrap.php
+++ bootstrap.php
@@ -1,14 +1,16 @@
 <?php
 
-// Autoloaders
-spl_autoload_register(function($className) {
-    // using two backslashes reoresents one backslash because it is an escape character
-    $path = __DIR__.'/lib/'.str_replace('\\', '/', $className).'.php';
+require __DIR__.'/vendor/autoload.php';
 
-    if (file_exists($path)) {
-        require $path;
-    }
-});
+// Autoloaders
+// spl_autoload_register(function($className) {
+//     // using two backslashes reoresents one backslash because it is an escape character
+//     $path = __DIR__.'/lib/'.str_replace('\\', '/', $className).'.php';
+
+//     if (file_exists($path)) {
+//         require $path;
+//     }
+// });
 
 // DB CONFIGURATION INFO
 $configuration = [
