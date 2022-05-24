a61f5d043c779c9a56f2ebc4a4684edc9811df0f
ljweston <lweston@ncees.org>
2022-05-24 10:45:46 -0400

    Moved header and footer to their own files

--- index.php
+++ index.php
@@ -31,35 +31,16 @@ if (isset($_GET['error'])) {
             $errorMessage = 'There was a disturbance in the force. Try again.';
     }
 }
+
+require 'layout/header.php'; 
 ?>
-
-<html>
-    <head>
-        <meta charset="utf-8">
-           <meta http-equiv="X-UA-Compatible" content="IE=edge">
-           <meta name="viewport" content="width=device-width, initial-scale=1">
-           <title>OO Battleships</title>
-
-           <!-- Bootstrap -->
-           <link href="css/bootstrap.min.css" rel="stylesheet">
-           <link href="css/style.css" rel="stylesheet">
-           <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-
-           <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-           <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-           <!--[if lt IE 9]>
-             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-           <![endif]-->
-    </head>
 
     <?php if ($errorMessage): ?>
         <div>
             <?php echo $errorMessage; ?>
         </div>
     <?php endif; ?>
-
-    <body>
+    
         <div class="container">
             <div class="page-header">
                 <h1>OO Battleships of Space</h1>
@@ -139,5 +120,5 @@ if (isset($_GET['error'])) {
                 </div>
             </div>
         </div>
-    </body>
-</html>
+
+<?php require 'layout/footer.php'; ?>
