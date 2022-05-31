4039eda53f5c49b40700a7657f769432e8e26f90
ljweston <lweston@ncees.org>
2022-05-31 10:39:48 -0400

    Added ship deletion logic

--- manage/ships/delete.php
+++ manage/ships/delete.php
@@ -2,11 +2,24 @@
 
 require '../layout/header.php';
 
+use Service\Container;
+
+// pass in id as a submitted value to pass to our query
 if (isset($_POST['deleteShip'])) {
     $id = $_POST['deleteShip'];
 
-    // call shipDelete function
+    $container = new Container($configuration);
+    $shipLoader = $container->getShipLoader();
+    // may want to check that the id is valid. If not we usually get an error back, but do not want to interrupt the program.
+    $shipLoader->deleteShip($id);
+} else {
+    echo '<h1> There is no ship to delete</h1>';
 }
 ?>
 
 <!-- Display a message to either re-route a user or to return to the battle menu -->
+<h2>This ship has been sent to the scrap yard.</h2>
+
+<a href="/index.php">Return to the battle!</a>
+
+<?php require '../layout/footer.php'?>
