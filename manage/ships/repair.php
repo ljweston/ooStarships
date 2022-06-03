c6decf1c967a7218abe89516c69750853741e099
ljweston <lweston@ncees.org>
2022-06-03 10:27:45 -0400

    Updated logic and added html for errors

--- manage/ships/repair.php
+++ manage/ships/repair.php
@@ -9,9 +9,19 @@ $id = isset($_GET['id']) ? $_GET['id'] : null;
 $container = new Container($configuration);
 $shipLoader = $container->getShipLoader();
 $ship = $shipLoader->findOneById($id);
-if ($ship === null) {
-    header('Location: /manage/ships/view.php');
+if ($ship != null) {
+    $shipLoader->repairShip($ship);
+    header('Location: /manage/ships/view.php?id='.$id);
 }
-$shipLoader->repairShip($ship);
+?>
 
-header('Location: /manage/ships/view.php?id='.$id);
+<div class="container">
+        <ul class="breadcrumb">
+            <li><a href="/">Home</a></li>
+            <li><a href="/manage/ships/index.php">ManageShips</a></li>
+            <li>Ship not found</li>
+        </ul>
+        <?php if ($ship === null): ?>
+            <h1> Ship Not Found </h1>
+        <?php endif;?>
+</div>
