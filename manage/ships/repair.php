4568c991f891f7446c7efeff84136de90e615812
ljweston <lweston@ncees.org>
2022-06-03 11:03:31 -0400

    Changed logic and added die statement for no ship

--- manage/ships/repair.php
+++ manage/ships/repair.php
@@ -9,19 +9,13 @@ $id = isset($_GET['id']) ? $_GET['id'] : null;
 $container = new Container($configuration);
 $shipLoader = $container->getShipLoader();
 $ship = $shipLoader->findOneById($id);
-if ($ship != null) {
-    $shipLoader->repairShip($ship);
-    header('Location: /manage/ships/view.php?id='.$id);
+if ($ship === null) {
+    echo '<h1>Ship Not Found</h1>';
+    echo '<a href="/manage/ships/index.php">ManageShips</a>';
+    die;
 }
+
+$shipLoader->repairShip($ship);
+header('Location: /manage/ships/view.php?id='.$id);
 ?>
 
-<div class="container">
-        <ul class="breadcrumb">
-            <li><a href="/">Home</a></li>
-            <li><a href="/manage/ships/index.php">ManageShips</a></li>
-            <li>Ship not found</li>
-        </ul>
-        <?php if ($ship === null): ?>
-            <h1> Ship Not Found </h1>
-        <?php endif;?>
-</div>
