e4ff2d7ca802ee609661cd86fd037ca3b565cdc4
ljweston <lweston@ncees.org>
2022-06-03 15:42:18 -0400

    Removed comments

--- manage/ships/index.php
+++ manage/ships/index.php
@@ -13,53 +13,53 @@ $shipLoader = $container->getShipLoader();
 $ships = $shipLoader->getShips();
 ?>
 
-        <div class="container">
-            <ul class="breadcrumb">
-                <li><a href="/">Home</a></li>
-                <li>ManageShips</li>
-            </ul>
-            <div class="page-header">
-                <h1>Manage Our Ships</h1>
-            </div>
-            <table class="table table-hover">
-                <caption><i class="fa fa-rocket"></i> View/ Edit These Ships</caption>
-                <a href="/" class="btn btn-md btn-primary pull-right">Return Home</a>
-                <thead>
-                    <tr>
-                        <th>Ship</th>
-                        <th>Weapon Power</th>
-                        <th>Jedi Factor</th>
-                        <th>Health</th>
-                        <th>Type</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($ships as $ship): ?>
-                        <tr>
-                            <td>
-                                <a href="/manage/ships/view.php?id=<?php echo $ship->getId();?>">
-                                    <?php echo $ship->getName(); ?>
-                                </a>
-                            </td>
-                            <td><?php echo $ship->getWeaponPower(); ?></td>
-                            <td><?php echo $ship->getJediFactor(); ?></td>
-                            <td><?php echo $ship->getCurrentHealth().'/'.$ship->getMaxHealth(); ?></td>
-                            <td><?php echo $ship->getType(); ?></td>
-                            <td>
-                                <?php if ($ship->isFunctional()) : ?>
-                                    <i class="Fa fa-sun-o"></i>
-                                <?php else : ?>
-                                    <i class="fa fa-cloud"></i>
-                                <?php endif; ?>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-            <div>
-                <a href="/manage/ships/new.php" class="btn btn-success">Add New Ship</a>
-            </div>
+    <div class="container">
+        <ul class="breadcrumb">
+            <li><a href="/">Home</a></li>
+            <li>ManageShips</li>
+        </ul>
+        <div class="page-header">
+            <h1>Manage Our Ships</h1>
         </div>
+        <table class="table table-hover">
+            <caption><i class="fa fa-rocket"></i> View/ Edit These Ships</caption>
+            <a href="/" class="btn btn-md btn-primary pull-right">Return Home</a>
+            <thead>
+                <tr>
+                    <th>Ship</th>
+                    <th>Weapon Power</th>
+                    <th>Jedi Factor</th>
+                    <th>Health</th>
+                    <th>Type</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($ships as $ship): ?>
+                    <tr>
+                        <td>
+                            <a href="/manage/ships/view.php?id=<?php echo $ship->getId();?>">
+                                <?php echo $ship->getName(); ?>
+                            </a>
+                        </td>
+                        <td><?php echo $ship->getWeaponPower(); ?></td>
+                        <td><?php echo $ship->getJediFactor(); ?></td>
+                        <td><?php echo $ship->getCurrentHealth().'/'.$ship->getMaxHealth(); ?></td>
+                        <td><?php echo $ship->getType(); ?></td>
+                        <td>
+                            <?php if ($ship->isFunctional()) : ?>
+                                <i class="Fa fa-sun-o"></i>
+                            <?php else : ?>
+                                <i class="fa fa-cloud"></i>
+                            <?php endif; ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <div>
+            <a href="/manage/ships/new.php" class="btn btn-success">Add New Ship</a>
+        </div>
+    </div>
 
 <?php require '../layout/footer.php';?>
